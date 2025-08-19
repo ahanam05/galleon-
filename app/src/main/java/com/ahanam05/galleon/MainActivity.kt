959a44d2cfed41f67f40a33a3cc81d48c22483ac
ahanam05 <ahanam05@gmail.com>
2025-08-20 01:13:30 +0530

    Add assets for Galleon logo

--- app/src/main/java/com/ahanam05/galleon/MainActivity.kt
+++ app/src/main/java/com/ahanam05/galleon/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GalleonTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Ahana",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
